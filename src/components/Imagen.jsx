97b15e88cdcc16b8182d3de07b855c1787a74dc8
ianbenja <iannicolasbenjamin@gmail.com>
2024-04-14 13:29:58 -0300

    Primera actualizacion nextui

---
+++ src/components/Imagen.jsx
@@ -0,0 +1,8 @@
+import { Avatar } from "@nextui-org/react";
+import ian from "./../assets/images/ian.jpg";
+
+const Imagen = () => {
+  return <Avatar src={ian} className="w-80 h-80 text-large" />;
+};
+
+export default Imagen;
